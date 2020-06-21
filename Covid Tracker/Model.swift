7de9d0a364c609de0c50a0676dea108343625c34
Argenis De La Rosa <oftherose91@gmail.com>
2020-06-21 02:02:04 -0400

    built the total data model

---
+++ Covid Tracker/Model.swift
@@ -0,0 +1,20 @@
+//
+//  Model.swift
+//  Covid Tracker
+//
+//  Created by Argenis De La Rosa on 6/21/20.
+//  Copyright © 2020 Argenis De La Rosa. All rights reserved.
+//
+
+import Foundation
+
+
+struct TotalData {
+    
+    let confirmed : Int
+    let critical : Int
+    let deaths : Int
+    let recovered : Int
+    
+}
+
