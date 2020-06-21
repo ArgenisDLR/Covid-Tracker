758114a26f47f3a20ba443aa3d5ab4dd875baa78
Argenis De La Rosa <oftherose91@gmail.com>
2020-06-21 01:33:18 -0400

    Initial Commit

---
+++ Covid Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid Tracker
+//
+//  Created by Argenis De La Rosa on 6/21/20.
+//  Copyright © 2020 Argenis De La Rosa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
